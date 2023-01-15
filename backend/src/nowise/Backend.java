f7efebaeecea54e203f790f53973c45958cb83f8
Anders Lövgren <andlov@nowise.se>
2023-01-15 22:09:06 +0100

    Start backend server from main.

--- backend/src/nowise/Backend.java
+++ backend/src/nowise/Backend.java
@@ -1,8 +1,32 @@
 package nowise;
 
+import java.io.IOException;
+import nowise.server.TrafficInformationServer;
+
 public class Backend {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        TrafficInformationServer server;
+        
+        for(String arg: args) {
+            System.out.println(arg);
+        }
+        
+        if (args.length > 1) {
+            String addr = args[0];
+            int port = Integer.parseInt(args[1]);
+
+            server = new TrafficInformationServer(addr, port);
+            server.start();
+        } else {
+            server = new TrafficInformationServer();
+            server.start();
+        }
+
+        System.out.println("Hit enter to shutdown server");
+        System.in.read();
+
+        server.stop();
     }
-    
 }
