f65b195babdd06decdf4e439285d4b5cd77af23e
Anders Lövgren <andlov@nowise.se>
2023-01-16 19:50:16 +0100

    Populate lines and bus stop list when clicked.

--- frontend/public_html/js/app.js
+++ frontend/public_html/js/app.js
@@ -1,12 +1,49 @@
-addEventListener("DOMContentLoaded", () => {
+async function init()
+{
     document.getElementById("update")
-            .addEventListener("click", () => {
-                fetch("http://localhost:8081/", {
+            .addEventListener("click", async () => {
+
+                const resp = await fetch("http://localhost:8081/", {
                     mode: "cors"
-                })
-                        .then((resp) => resp.json())
-                        .then((json) => {
-                            console.log(json);
-                        });
+                });
+                const json = await resp.json();
+
+                const lineSect = document.getElementById("line-sect");
+                const lineList = document.getElementById("line-list");
+
+                const stopSect = document.getElementById("stop-sect");
+                const stopList = document.getElementById("stop-list");
+                const stopHead = document.getElementById("stop-head");
+
+                lineList.innerHTML = "";
+
+                for (const index in json) {
+                    const line = json[index];
+                    const item = document.createElement("li");
+
+                    item.innerHTML =
+                            "<span>Linje " + line.number + "</span>" +
+                            "<span class=\"w3-badge w3-indigo w3-margin-left\">" + line.stops.length + "</span>";
+
+                    item.addEventListener("click", () => {
+                        stopHead.innerHTML = "Hållplatser (Linje " + index + ")";
+                        stopList.innerHTML = "";
+                        stopSect.style.display = "";
+
+                        for (const index in line.stops) {
+                            const stop = line.stops[index];
+                            const item = document.createElement("li");
+
+                            item.innerHTML = stop.stopPointName;
+                            stopList.appendChild(item);
+                        }
+                    });
+
+                    lineList.appendChild(item);
+                }
+
+                lineSect.style.display = "";
             });
-});
+}
+
+addEventListener("DOMContentLoaded", () => init());
